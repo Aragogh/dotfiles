a01c8613fc6d44ce766b6ed65883ecde4dca734b
Aragogh <avc.techalchemy@gmail.com>
2021-06-01 10:24:24 +0530

    dotfiles

---
+++ .emacs.d/org-caldav-90f53e8.el
@@ -0,0 +1,8 @@
+;; This is the sync state from org-caldav
+;; calendar-id: self
+;; Do not modify this file.
+
+(setq org-caldav-event-list
+'nil)
+
+(setq org-caldav-previous-files '("~/storage/Nextcloud/Notes/Org/todo.org" "~/storage/Nextcloud/Notes/Org/notes.org" "~/storage/Nextcloud/Notes/Org/caldav.org"))
--- .emacs.d/org-caldav-be5a6c8.el
+++ .emacs.d/org-caldav-be5a6c8.el
@@ -3,16 +3,20 @@
 ;; Do not modify this file.
 
 (setq org-caldav-event-list
-'(("1d9f01ae-2847-4b46-ac2d-9a63d5e0473c" "05296f1d15ad0a687dd8e2ff17de8624" "ed82213e9ad2ec3cddfd6e6c76e20aab" 2 synced) 
-("042427ca-7a57-414b-8863-b1d1ffeb9eaf" "4a91c56cc7abf09d509d37fc1404819d" "08d595e0426e09a6be48676d371d645c" 4 changed-in-org) 
-("7ef93801-e9c0-4f4f-9d28-cd2928649651" "e7454bd2af884e527255a018025d92fc" "6d56543dde3ecd3ee1a3c313f94767b2" 1 changed-in-org) 
+'(("042427ca-7a57-414b-8863-b1d1ffeb9eaf" "4a91c56cc7abf09d509d37fc1404819d" "08d595e0426e09a6be48676d371d645c" 4 synced) 
+("7ef93801-e9c0-4f4f-9d28-cd2928649651" "e7454bd2af884e527255a018025d92fc" "6d56543dde3ecd3ee1a3c313f94767b2" 1 synced) 
 ("0813aafe-e768-4ba1-813f-7ab9c74fb388" "95cf32978c81e3d2e3f25bb376b08eb6" "3b2bc2d5424e5234e1031c6fc57210f4" 1 synced) 
 ("cd6b2011-31f4-4259-97e2-7c731fe4628d" "47bcf77bdc8b9ab4ee385595e9ee0673" "efb0b486fc14e1fda47b996267c6fdd0" nil synced) 
 ("fe01da81-ee6a-4c98-a954-59a15c8a2f56" "498df24109eab22225605c6a7fe29605" "f648f773ccf03b5ddfaa53452dd3acca" nil synced) 
-("6f1757c9-2c14-4b52-90b4-a4eb47dddbe3" "24f5799d56babc542158922fb3f4ff20" "dae2788b232aadbf04dbbb22733187ea" nil deleted-in-cal) 
-("27f86539-9d1f-4713-9141-b347c6f7db02" "aeb8fd64c78ddc25a13a1cdf019b56c3" "c8b910a007d2bb8f4d80cec1a2985eb1" nil synced) 
+("27f86539-9d1f-4713-9141-b347c6f7db02" "e4e3a6250e703bf534ca372bc414a9fc" "184bd5fc8f252cf2e2f5aa641200508c" 1 synced) 
 ("3435264b-727a-406d-9b11-3b2c20905f03" "a6b442dd4b214444e9eb2000e7ec57a1" "e15fe98b7cfc5bb23565377da3d8a084" 2 synced) 
-("2fcdb6d8-6db5-49cd-b923-a1be950c1a7b" "b80b5aed20fc5fadd7268765f891ad7e" "f32ceeb39c2b6303ce2e5fad363feac9" 2 changed-in-org) 
-("92172646-84b6-42a2-954b-85159c5a782a" "9cbbeebec7db55b04995f4466b463414" "9301431b96f663e2954d40b8df859e4e" nil synced)))
+("2fcdb6d8-6db5-49cd-b923-a1be950c1a7b" "b80b5aed20fc5fadd7268765f891ad7e" "f32ceeb39c2b6303ce2e5fad363feac9" 2 synced) 
+("7b44aec0-08a0-4296-b6ba-f90e928c1aae" "f2723d9c36c3a037d57e1eb0f7338158" "8c89cbca826c7c6bf6a7b783271bc71d" nil synced) 
+("0012b108-043c-435d-a87d-5c4f7c431bae" "1e5784273adc9d806353c905e7a32900" "e2044a270c20291bd12d6dfdcea09906" nil synced) 
+("e4bbd489-902f-485d-a94e-d33c86a3946d" "4570af62cb5942f0366a2cebdfcbf678" "18220904439b9857a0e686f5a1cac124" nil synced) 
+("c46e1f39-b335-465f-ac9c-8ff366d4e842" "527e65ffcca0129f5cfba1cebd436abe" "fad65412705ad6b4e82d8327075b152a" nil synced) 
+("2b25d061-e040-4ba7-ad80-8048c1eede8d" "25c7971d5a0a10778c3aa1ac3be79c70" "2a1546245621a3177e3027b85515e828" nil synced) 
+("ad81cf46-1ff1-4539-ba70-2fa1f77abae9" "b251cc29a31e6f1e882ae25d7324e9d1" "c75b37821166f78c143b836a4e823cf7" nil synced) 
+("cc5dd714-a089-4c97-8eac-1d8bdb18df7f" "c07b2750e1a6f3cae3e14428f59aadbd" "aa19b91ba57fe1d9217a90f287d36364" nil synced)))
 
 (setq org-caldav-previous-files '("~/storage/Nextcloud/Notes/Org/todo.org" "~/storage/Nextcloud/Notes/Org/notes.org" "~/storage/Nextcloud/Notes/Org/caldav.org"))
